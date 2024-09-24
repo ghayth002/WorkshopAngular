72fc5053dc1f18746160b4fefba9c8a21b018ff4
Ghayth <103043245+ghayth002@users.noreply.github.com>
2024-09-24 10:44:01 +0100

    workshop3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListcategoryComponent } from './pages/listcategory/listcategory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListcategoryComponent
   ],
   imports: [
     BrowserModule,
